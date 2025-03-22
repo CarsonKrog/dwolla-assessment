8ed8d4f2c4c397c5eb7bc84b03307e1a9e693696
CarsonKrog <you@example.com>
2025-03-22 17:37:48 -0500

    Finished table

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,10 @@ import { Box } from '@mui/material';
 // Icon for Add Customer button
 import { AddRounded } from '@mui/icons-material';
 
+import React from 'react';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Typography, Button, Stack } from '@mui/material';
+import AddIcon from '@mui/icons-material/Add';
+
 export type Customer = {
   firstName: string;
   lastName: string;
@@ -39,19 +43,40 @@ const Home = () => {
         <title>Dwolla | Customers</title>
       </Head>
       <main>
-        <Box>
+        <div className="table-container">
           {isLoading && <p>Loading...</p>}
           {error && <p>Error: {error.message}</p>}
           {data && (
-            <ul>
-              {data.map(customer => (
-                <li key={customer.email}>
-                  {customer.firstName} {customer.lastName}
-                </li>
-              ))}
-            </ul>
+            <>
+              <TableContainer component={Paper}>
+                <Table>
+                  <TableHead>
+                    <TableRow>
+                      <TableCell colSpan={2} className="table-cell-no-border">
+                        <div className="table-header">
+                          <Typography variant="h6">{data.length} Customers</Typography>
+                          <Button variant="contained" endIcon={<AddIcon />}>Add Customer</Button>
+                        </div>
+                      </TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableCell>Name</TableCell>
+                      <TableCell>Email</TableCell>
+                    </TableRow>
+                  </TableHead>
+                  <TableBody>
+                    {data.map((customer) => (
+                      <TableRow key={customer.email}>
+                        <TableCell>{customer.firstName} {customer.lastName}</TableCell>
+                        <TableCell>{customer.email}</TableCell>
+                      </TableRow>
+                    ))}
+                  </TableBody>
+                </Table>
+              </TableContainer>
+            </>
           )}
-        </Box>
+        </div>
       </main>
     </>
   );
